596986f160526c5e953ca75dd159c76282a802ef
Michael Prasuhn <mike@mikeyp.net>
2020-02-17 15:45:09 -0500

    Updating to use react hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,57 @@
 import { NextPage } from 'next'
 import fetch from 'unfetch'
-import useSWR from 'swr'
-
-const fetcher = async (path: string) => {
-  const res = await fetch(path)
-  const json = await res.json()
-  return json
-}
+import moment from 'moment'
+import { useState, useEffect } from 'react'
 
 const Page: NextPage = (children) => {
-  const { data, error } = useSWR('/api/weather', fetcher)
+  const [loaded, setLoaded] = useState(false)
+  const [updatedAt, setUpdatedAt] = useState('')
+  const [forecast, setForecast] = useState({ currently:  { time: 0, temperature: 0} })
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-  return <div>Current temp: {data.currently.temperature}</div>
+  const updateForecast = () => {
+    console.log("updating forecast")
+    fetch('/api/weather')
+      .then(r => r.json())
+      .then(data => {
+        setForecast(data)
+        setUpdatedAt(getFormattedDate(data.currently.time));
+      })
+      .then(() => setLoaded(true))
+  }
+
+  useEffect(() => {
+    updateForecast()
+    const updateInterval = setInterval(() => {
+      updateForecast()
+    }, 1000 * 60)
+  }, [])
+
+  useEffect(() => {
+    if (loaded) {
+      const interval = setInterval(() => {
+        setUpdatedAt(getFormattedDate(forecast.currently.time));
+      }, 4000)
+
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [forecast, loaded])
+
+  const getFormattedDate = (time) => {
+    return moment.unix(time).fromNow()
+  }
+
+  return (
+    <div>
+      { loaded &&
+        <>
+          <div>Current temp: { forecast.currently.temperature }</div>
+        <div>Forecast age: { updatedAt }</div>
+        </>
+      }
+    </div>
+  )
 }
 
 
